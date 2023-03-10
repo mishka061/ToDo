96ac494055e25d6a46256796a6392c49216aab05
Алена Котова <mishka061@rambler.ru>
2023-03-10 11:25:17 +0300

    add

---
+++ main.js
@@ -0,0 +1,4 @@
+let form = document.querySelector('#form') //Получаем форму
+let inPut = document.querySelector('#input') //Получаем кнопку
+let taskList = document.querySelector('#taskList') //Получаем ul
+let emptylist = document.querySelector('#emptyList') //Получаем li
